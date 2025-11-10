45585184d6013ebfee04662a178501697b8c51aa
doladoli <ismearab@gmail.com>
2025-11-10 18:05:08 +0800

    Implement conditional inclusion in index.php
    Add PHP logic to include portal_main_ct.php based on GET parameters.

--- index.php
+++ index.php
@@ -1,3 +1,8 @@
-<form method="post" action="ct/handlefatwa.php">
-<button>Login</button>
-</form>
+<?php
+
+$mod=$_GET['module'];
+$app=$_GET['app'];
+
+if (($mod=='staf') && ($app=='CR')) {
+    include("portal_main_ct.php");  
+}
